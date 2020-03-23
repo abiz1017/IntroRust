07a088995d21a2a5fcb9697407c2dec9caacaf18
Alex <abiz@ucsd.edu>
2020-03-23 10:11:17 -0700

    first

---
+++ target/debug/deps/say_my_name-04184074daef766d.d
@@ -0,0 +1,5 @@
+/Users/alexanderbiz/projects/rust/say-my-name/target/debug/deps/say_my_name-04184074daef766d: src/main.rs
+
+/Users/alexanderbiz/projects/rust/say-my-name/target/debug/deps/say_my_name-04184074daef766d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/say-my-name.d
@@ -0,0 +1,1 @@
+/Users/alexanderbiz/projects/rust/say-my-name/target/debug/say-my-name: /Users/alexanderbiz/projects/rust/say-my-name/src/main.rs
